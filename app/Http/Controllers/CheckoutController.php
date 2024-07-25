42c0d6df219e24f2aad43f7eec377e5a10a4ee20
Fajarwz <fajarwindhuzulfikar@gmail.com>
2024-07-25 10:42:02 +0700

    chore: midtrans config to env

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -187,10 +187,10 @@ class CheckoutController extends Controller
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
+        \Midtrans\Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
         // Set sanitization on (default)
-        \Midtrans\Config::$isSanitized = true;
+        \Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
         // Set 3DS transaction for credit card to true
-        \Midtrans\Config::$is3ds = true;
+        \Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
     }
 }
